e8622f48ef6976470dc3d259816b5164aafccebd
Banu Prakash <banuprakash@furnify.in>
2023-03-18 10:35:32 +0530

    FEAT: GET Users api add

---
+++ src/pages/api/users.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma';
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+
+        try {
+            const result = await prisma.pr_users.findMany({
+                include: {
+                    paragraph: true
+                }
+            });
+            
+            res.status(200).json(result);
+
+        } catch (error) {
+            console.log("error", error)
+            res.status(500).json({ message: "Something Went Wrong" });
+        }
+
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+}
