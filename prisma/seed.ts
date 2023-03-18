25fc6192f758073a849ce4bf5493d9541e3e1c44
Banu Prakash <banuprakash@furnify.in>
2023-03-18 10:33:45 +0530

    FEAT: Database seed data add and database url add

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const initialSeedData: Prisma.pr_usersCreateInput[] = [
+    {
+        firstName: "admin",
+        email: "admin@admin.com",
+        mobile: "9876543210",
+        crtBy: "admin",
+        updBy: "admin",
+        paragraph: {
+            create: <Prisma.pr_story_paragraphCreateInput[]>[
+                {
+                    paragraph: "testing a new story to check if al working fine",
+                    crtBy: "admin",
+                    updBy: "admin"
+                }
+            ]
+        }
+    },
+    {
+        firstName: "guest",
+        email: "guest@admin.com",
+        mobile: "9876543210",
+        crtBy: "guest",
+        updBy: "guest",
+        paragraph: {
+            create: <Prisma.pr_story_paragraphCreateInput[]>[
+                {
+                    paragraph: "testing a new story to check if al working fine by guest",
+                    crtBy: "guest",
+                    updBy: "guest"
+                }
+            ]
+        }
+    }
+]
+
+async function main() {
+    console.log(`Start seeding ...`)
+    for (const u of initialSeedData) {
+        const user = await prisma.pr_users.create({
+            data: u,
+        })
+        console.log(`Created user with id: ${user.userId}`)
+    }
+    console.log(`Seeding finished.`)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
