36b8c5a9c81499a6c55f0b75667d1280a66065b7
Banu Prakash <banuprakash@furnify.in>
2023-03-19 11:28:42 +0530

    FEAT: UI/UX improvements for the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { useEffect } from "react";
 import axios from "axios";
+import Header from "@/components/header";
+import Body from "@/components/body";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-
   const getStories = async () => {
     const data = await axios.get("/api/story");
     console.log(data);
-  }
+  };
   const getUsers = async () => {
     const data = await axios.get("/api/users");
     console.log(data);
-  }
+  };
   const addStory = async () => {
     const data = await axios.post("/api/story/add");
     console.log(data);
-  }
-  
+  };
+
   return (
     <>
+      <Header />
+      <Body />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-     <button onClick={getStories}>Get Stories</button>
-     <button onClick={getUsers}>Get Users</button>
-     <button onClick={addStory}>Add Story</button>
+      <button onClick={getStories}>Get Stories</button>
+      <button onClick={getUsers}>Get Users</button>
+      <button onClick={addStory}>Add Story</button>
     </>
   );
 }
