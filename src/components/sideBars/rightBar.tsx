6c33bc1a5d9691741203c895c2cf449ee588c470
Banu Prakash <banuprakash@furnify.in>
2023-03-19 11:27:01 +0530

    FEAT: New Sidebar components add

---
+++ src/components/sideBars/rightBar.tsx
@@ -0,0 +1,13 @@
+import NewStoryButton from "./newStoryButton";
+import TopUsers from "./topUsers";
+
+const RightBar = () => {
+  return (
+    <div className="p-4">
+      <NewStoryButton />
+      <TopUsers />
+    </div>
+  );
+};
+
+export default RightBar;
