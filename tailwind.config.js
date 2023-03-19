68b743587b60bea7f27f42f2a2eff6e07eeafc56
Banu Prakash <banuprakash@furnify.in>
2023-03-19 11:24:01 +0530

    CONFIG: Tailwind extra config add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      bodyBg: "#f1f5f9",
+      white: "#ffffff",
+      black: "#464646",
+      subBlack: "#8b8b8b",
+      green: "#70e000",
+      lightGreen: "#b9f67ca1",
+      darkGreen: "#004B23",
+      midGreen: "#38B000",
+      lightBlack: "#4646468c",
+      red: "#ff4d5f",
+      lightGrey: "#ece7e7",
+      blue: "#3d5af1",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
