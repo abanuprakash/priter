b98d20d4be0f410772f6b3ec6ac0ff916c2acc11
Banu Prakash <banuprakash@furnify.in>
2023-04-14 18:36:55 +0530

    FEAT: Individual story pages add

---
+++ src/pages/story/[id].tsx
@@ -0,0 +1,42 @@
+import { Story } from "@/_types/story";
+import FullStoryDetails from "@/components/singleStory/FullStoryDetails";
+import { useAppStoryContext } from "@/providers/StoryContext";
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+
+interface IProps {
+  story: Story;
+}
+
+const StoryDetailsPage = ({ story }: IProps) => {
+  const { setCurrentStories, setRightStories } = useAppStoryContext();
+
+  useEffect(() => {
+    handleStories();
+  }, [story]);
+
+  const handleStories = async () => {
+    await setCurrentStories(story);
+    setRightStories(story.childParagraphs);
+  };
+
+  return (
+    <div className="mt-20 max-w-7xl mx-auto ">
+      <FullStoryDetails />
+    </div>
+  );
+};
+
+export default StoryDetailsPage;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.params!;
+  const response = await axios.get(`http://localhost:3000/api/story/${id}`);
+
+  return {
+    props: {
+      story: response.data,
+    },
+  };
+};
