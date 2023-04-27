f9a8d7ca5eb63928c2a5f05b0a612edff7964096
Banu Prakash <banuprakash@furnify.in>
2023-04-27 22:28:03 +0530

    REFACT: Post API refactor

--- src/pages/api/story/add.ts
+++ src/pages/api/story/add.ts
@@ -2,18 +2,13 @@ import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    const newParagraph = {
-        paragraph: "testing a new children story to check if al working fine",
-        crtBy: "admin",
-        updBy: "admin",
-        parentId: 1,
-        userId: 1
-    }
+
     if (req.method === 'POST') {
+        console.log(req.body)
 
         try {
-            const result = await prisma.pr_story_paragraph.create({ data: newParagraph });
-            
+            const result = await prisma.pr_story_paragraph.create({ data: req.body });
+
             res.status(200).json(result);
 
         } catch (error) {
