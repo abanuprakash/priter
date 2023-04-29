04107b529fbce578b92fd12c75ff5094a2e620de
Banu Prakash <banuprakash@furnify.in>
2023-04-29 12:02:15 +0530

    FEAT: Mobile footer menu fix

--- src/_const/mainMenus.ts
+++ src/_const/mainMenus.ts
@@ -1,7 +1,7 @@
 export const MainMenus = [
-    { id: 1, key: "home", label: "Home", icon: "icon-home", forMobile: true, forWeb: true },
-    { id: 2, key: "explore", label: "Explore Topics", icon: "icon-compass", forMobile: true, forWeb: true },
-    { id: 3, key: "new", label: "Add New", icon: "icon-plus-circle", forMobile: true, forWeb: false },
-    { id: 4, key: "my", label: "My Topics", icon: "icon-at-sign", forMobile: true, forWeb: true },
-    { id: 5, key: "bookmark", label: "Bookmarks", icon: "icon-bookmark", forMobile: true, forWeb: true },
+    { id: 1, key: "home", label: "Home", icon: "icon-home", forMobile: true, forWeb: true, isSheet: false },
+    { id: 2, key: "explore", label: "Explore Topics", icon: "icon-compass", forMobile: true, forWeb: true, isSheet: false },
+    { id: 3, key: "new", label: "Add New", icon: "icon-plus-circle", forMobile: true, forWeb: false, isSheet: true },
+    { id: 4, key: "my", label: "My Topics", icon: "icon-at-sign", forMobile: true, forWeb: true, isSheet: false },
+    { id: 5, key: "bookmark", label: "Bookmarks", icon: "icon-bookmark", forMobile: true, forWeb: true, isSheet: false },
 ];
--- src/components/footer/bottomFixedMenu.tsx
+++ src/components/footer/bottomFixedMenu.tsx
@@ -1,27 +1,36 @@
 import { MainMenus } from "@/_const/mainMenus";
 import { useState } from "react";
+import NewStoryButton from "../sideBars/newStoryButton";
 
 const BottomFixedMenu = () => {
   const [active, setActive] = useState("home");
   const menus = MainMenus.filter((menu) => menu.forMobile === true);
 
+  const handleClose = () => {
+    setActive("home");
+  };
+
   return (
-    <nav className="fixed -bottom-[1px] h-16 w-full bg-white pb-1">
-      <section className="p-4 pt-3 grid grid-flow-row grid-cols-5 gap-3 mb-1 justify-items-center">
-        {menus.map((menu) => (
-          <div
-            key={menu.id}
-            className={`flex items-center outline-none justify-center cursor-pointer relative group mb-2 rounded-full p-1 w-11 h-11 hover:bg-lightGreen  ${
-              active === menu.key ? "bg-lightGreen" : "text-lightBlack"
-            }`}
-            onClick={() => setActive(menu.key)}
-          >
-            <i className={`${menu.icon} feather mr-3 text-2xl pl-3 `}></i>
-            
-          </div>
-        ))}
-      </section>
-    </nav>
+    <>
+      <nav className="fixed -bottom-[1px] h-16 w-full bg-white pb-1">
+        <section className="p-4 pt-3 grid grid-flow-row grid-cols-5 gap-3 mb-1 justify-items-center">
+          {menus.map((menu) => (
+            <div
+              key={menu.id}
+              className={`flex items-center outline-none justify-center cursor-pointer relative group mb-2 rounded-full p-1 w-11 h-11 hover:bg-lightGreen  ${
+                active === menu.key ? "bg-lightGreen" : "text-lightBlack"
+              }`}
+              onClick={() => setActive(menu.key)}
+            >
+              <i className={`${menu.icon} feather mr-3 text-2xl pl-3 `}></i>
+            </div>
+          ))}
+        </section>
+      </nav>
+      {active === "new" && (
+        <NewStoryButton isDefaultOpen={true} handleClose={handleClose} />
+      )}
+    </>
   );
 };
 
