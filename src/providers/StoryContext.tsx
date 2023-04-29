e5f0b3e2f707564b9c4d3a4763dda82849738070
Banu Prakash <banuprakash@furnify.in>
2023-04-29 12:00:21 +0530

    FEAT: new state add

--- src/providers/StoryContext.tsx
+++ src/providers/StoryContext.tsx
@@ -6,10 +6,16 @@ interface initialState {
   leftSideList: Story[];
   rightSideList: Story[];
   currentStory: Story[];
+  showRightSidebar: boolean;
+  showLeftSidebar: boolean;
   setStories: (story: Story[]) => void;
   setLeftSideStories: (story: Story[]) => void;
   setRightStories: (story: Story[]) => void;
-  setCurrentStories: (story: Story) => void;
+  setCurrentStories: (story: Story, isFilterStory: boolean) => void;
+  setCurrentStoriesAsArray: (story: Story[]) => void;
+  setInitialCurrentStory: (story: Story) => void;
+  setShowRightSideBar: (isOpen: boolean) => void;
+  setShowLeftSideBar: (isOpen: boolean) => void;
 }
 
 const initialValue: initialState = {
@@ -17,10 +23,16 @@ const initialValue: initialState = {
   leftSideList: [{} as Story],
   rightSideList: [{} as Story],
   currentStory: [{} as Story],
+  showRightSidebar: false,
+  showLeftSidebar: false,
   setStories: (): void => undefined,
   setLeftSideStories: (): void => undefined,
   setRightStories: (): void => undefined,
   setCurrentStories: (): void => undefined,
+  setCurrentStoriesAsArray: (): void => undefined,
+  setInitialCurrentStory: (): void => undefined,
+  setShowRightSideBar: (): void => undefined,
+  setShowLeftSideBar: (): void => undefined,
 };
 
 type Props = {
@@ -34,28 +46,42 @@ export const StoryProvider = ({ children }: Props) => {
   const [leftSideList, setLeftSideList] = useState<Story[]>([]);
   const [rightSideList, setRightSideList] = useState<Story[]>([]);
   const [currentStory, setCurrentStory] = useState<Story[]>([]);
+  const [showRightSidebar, setShowRightSideBar] = useState(false);
+  const [showLeftSidebar, setShowLeftSideBar] = useState(false);
 
   const setStories = (story: Story[]) => {
     setStoriesList(story);
   };
 
   const setLeftSideStories = (story: Story[]) => {
-    setLeftSideList(story);
+    setLeftSideList(() => story);
   };
 
   const setRightStories = (newStories: Story[]) => {
-    setRightSideList(newStories);
+    setRightSideList(() => newStories);
   };
 
-  const setCurrentStories = (newStory: Story) => {
+  const setInitialCurrentStory = (newStories: Story) => {
+    setCurrentStory(() => [newStories]);
+  };
+
+  const setCurrentStoriesAsArray = (stories: Story[]) => {
+    setCurrentStory(stories);
+  };
+
+  const setCurrentStories = (newStory: Story, isFilterStory: boolean) => {
     if (currentStory.some((currStory) => currStory.id === newStory.id)) {
       return false;
     }
 
-    const removeFromCurrentStory = [...currentStory].filter(
-      (currStory) =>
-        !leftSideList.some((leftStory) => leftStory.id === currStory.id)
-    );
+    let removeFromCurrentStory = [...currentStory];
+
+    if (isFilterStory) {
+      removeFromCurrentStory = removeFromCurrentStory.filter(
+        (currStory) =>
+          !leftSideList.some((leftStory) => leftStory.id === currStory.id)
+      );
+    }
 
     setCurrentStory(() => [...removeFromCurrentStory, newStory]);
   };
@@ -65,10 +91,16 @@ export const StoryProvider = ({ children }: Props) => {
     leftSideList,
     rightSideList,
     currentStory,
+    showRightSidebar,
+    showLeftSidebar,
     setLeftSideStories,
     setRightStories,
     setStories,
     setCurrentStories,
+    setCurrentStoriesAsArray,
+    setInitialCurrentStory,
+    setShowRightSideBar,
+    setShowLeftSideBar,
   };
   return (
     <StoryContext.Provider value={value}>{children}</StoryContext.Provider>
