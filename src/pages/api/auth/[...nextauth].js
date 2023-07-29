2f643088f5538bd80cacc4ec6d5fa4cd47176279
Banu Prakash <banuprakash@furnify.in>
2023-07-29 17:26:27 +0530

    Next auth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,12 +6,12 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      clientId: '78717558144-07r9e7phmns23pun8ngni5bueu8gif8t.apps.googleusercontent.com',
-      clientSecret: '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'
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+
+  secret: process.env.SECRET // SECRET env variable 
 }
 
 export default NextAuth(authOptions)
