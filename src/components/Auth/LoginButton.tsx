232d0c63655dd5621934d6d3834519e8fb9f192c
Banu Prakash <banuprakash@furnify.in>
2023-07-29 18:16:41 +0530

    FEAT: Login button handle

--- src/components/Auth/LoginButton.tsx
+++ src/components/Auth/LoginButton.tsx
@@ -1,18 +1,19 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const LoginButton = () => {
-    const { data: session } = useSession()
-    if (session) {
-        return (
-            <>
-                Signed in as {session?.user?.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        )
-    }
+    const { data: session } = useSession();
+    console.log(session)
+    // if (session) {
+    //     return (
+    //         <>
+    //             Signed in as {session?.user?.email} <br />
+    //             <button onClick={() => signOut()}>Sign out</button>
+    //         </>
+    //     )
+    // }
     return (
         <>
-            Not signed in <br />
+            {/* Not signed in <br /> */}
             <button onClick={() => signIn('google')}>Sign in</button>
         </>
     )
