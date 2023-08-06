a6ea60e8d71fb8c80b0d31232c99cd42c06d15cf
Banu Prakash <banuprakash@furnify.in>
2023-08-06 17:02:44 +0530

    FEAT: Adapter add and seed remove

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth"
 // import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "../../../lib/prisma"
 
 export const authOptions = {
   // Configure one or more authentication providers
+  
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
