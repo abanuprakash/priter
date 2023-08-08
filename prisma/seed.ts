92e5c418629271034bd16dfaab01e1870f8f4d6e
Banu Prakash <banuprakash@furnify.in>
2023-08-08 23:27:33 +0530

    FEAT: Schema update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,61 +1,38 @@
-// import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// const initialSeedData: Prisma.pr_usersCreateInput[] = [
-//     {
-//         firstName: "admin",
-//         email: "admin@admin.com",
-//         mobile: "9876543210",
-//         crtBy: "admin",
-//         updBy: "admin",
-//         paragraph: {
-//             create: <Prisma.pr_story_paragraphCreateInput[]>[
-//                 {
-//                     paragraph: "testing a new story to check if al working fine",
-//                     title: "testing the new story",
-//                     crtBy: "admin",
-//                     updBy: "admin"
-//                 }
-//             ]
-//         }
-//     },
-//     {
-//         firstName: "guest",
-//         email: "guest@admin.com",
-//         mobile: "9876543210",
-//         crtBy: "guest",
-//         updBy: "guest",
-//         paragraph: {
-//             create: <Prisma.pr_story_paragraphCreateInput[]>[
-//                 {
-//                     paragraph: "testing a new story to check if al working fine by guest",
-//                     title: "testing the new story",
-//                     crtBy: "guest",
-//                     updBy: "guest"
-//                 }
-//             ]
-//         }
-//     }
-// ]
+const initialSeedData: Prisma.UserCreateInput[] = [
+    {
+        id: 'guestuser001',
+        name: "guest",
+        email: "guest@guest.com",
+    },
+    {
+        id: 'adminuser001',
+        name: "admin",
+        email: "admin@admin.com",
+    }
 
-// async function main() {
-//     console.log(`Start seeding ...`)
-//     for (const u of initialSeedData) {
-//         const user = await prisma.pr_users.create({
-//             data: u,
-//         })
-//         console.log(`Created user with id: ${user.userId}`)
-//     }
-//     console.log(`Seeding finished.`)
-// }
+]
 
-// main()
-//     .then(async () => {
-//         await prisma.$disconnect()
-//     })
-//     .catch(async (e) => {
-//         console.error(e)
-//         await prisma.$disconnect()
-//         process.exit(1)
-//     })
+async function main() {
+    console.log(`Start seeding ...`)
+    for (const u of initialSeedData) {
+        const user = await prisma.user.create({
+            data: u,
+        })
+        console.log(`Created user with id: ${user.id}`)
+    }
+    console.log(`Seeding finished.`)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
