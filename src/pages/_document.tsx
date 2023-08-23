75744bb926942ca6fffb950fd126bd9df9249b54
Banu Prakash <banuprakash@furnify.in>
2023-08-23 22:47:10 +0530

    FEAT: Font Lato update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,39 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@200;300;400;500;600;700&display=swap"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@200;300;400;500;600;700&display=swap"
+          />
+        </noscript>
+
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+                </style>
+                  <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Lato:wght@200;300;400;500;600;700&display=swap"
+                    media="print"
+                    onload="this.media = 'all';"
+                  />
+                <style>
+              `,
+          }}
+        ></style>
+      </Head>
       <body className='bg-bodyBg lg:h-screen lg:overflow-hidden'>
         <Main />
         <NextScript />
